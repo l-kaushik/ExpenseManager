ec0db888e1f20d8d4189678121e37ffe8ca46bb1
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2025-09-14 18:54:41 +0530

    refactor: Replaced UserTest to UserIntegratedTest and reimplemented UserTest with BaseJpaTest
    - Added assertions for required field in UserTest.

---
+++ src/test/java/in/lokeshkaushik/expensemanager/model/user/UserIntegratedTest.java
@@ -0,0 +1,110 @@
+package in.lokeshkaushik.expensemanager.model.user;
+
+import in.lokeshkaushik.expensemanager.model.account.Account;
+import in.lokeshkaushik.expensemanager.model.account.BankBranch;
+
+import in.lokeshkaushik.expensemanager.utils.BaseJpaTest;
+import org.junit.jupiter.api.*;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserIntegratedTest extends BaseJpaTest {
+
+    @Test
+    void testPersistAndFindUser() {
+        FullName fullName = FullName.builder()
+                .firstName("Ian")
+                .middleName("John")
+                .lastName("Marr")
+                .build();
+
+        Address address1 = Address.builder()
+                .city("Angeles Los")
+                .street("456 Elm Street")
+                .state("AC")
+                .zipCode("90001")
+                .build();
+
+        Address address2 = Address.builder()
+                .city("Bournemouth")
+                .street("73 Android Road")
+                .state("AC")
+                .zipCode("70002")
+                .build();
+
+        UserAuth auth = UserAuth.builder()
+                .passwordHash("asfl2kneorinfosknosi39")
+                .email("example@email.com")
+                .salt("djsfoiej@#$9df469sdfsod")
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+
+        UserInfo userInfo = UserInfo.builder()
+                .fullName(fullName)
+                .addresses(List.of(address1, address2))
+                .brithDate(new Date(1757836172))
+                .build();
+
+        BankBranch bankBranch = BankBranch.builder()
+                .IFSCCode("State Bank of USA")
+                .branchName("Conn aught Place")
+                .branchCode("00691")
+                .build();
+
+        Account account1 = Account.builder()
+                .accountNumber(1122334455)
+                .bankBranch(bankBranch)
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+
+        Account account2 = Account.builder()
+                .accountNumber(1234123412)
+                .bankBranch(bankBranch)
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+
+        User user = User.builder()
+                .username("Lokesh")
+                .userAuth(auth)
+                .userInfo(userInfo)
+                .accounts(List.of(account1, account2))
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+
+        fullName.setUserInfo(userInfo);
+        address1.setUserInfo(userInfo);
+        address2.setUserInfo(userInfo);
+        auth.setUser(user);
+        bankBranch.setAccounts(List.of(account1, account2));
+        account1.setUser(user);
+        account2.setUser(user);
+        userInfo.setUser(user);
+
+        entityTransaction.begin();
+        entityManager.persist(bankBranch);
+        entityManager.persist(user);
+        entityTransaction.commit();
+
+        entityManager.clear(); // clear cache so we fetch from DB
+
+        User found = entityManager.find(User.class, user.getUserId());
+
+        assertNotNull(found);
+        assertEquals(user, found);
+        assertEquals("Lokesh", found.getUsername());
+        assertNotNull(found.getUserAuth());
+        assertNotNull(found.getUserInfo());
+        assertEquals(2, found.getAccounts().size());
+//        assertEquals(1, found.getExpenses().size());
+    }
+}
