239f71dec72c5a2170491d8f83970cd381d977b9
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2025-09-14 18:53:16 +0530

    feat: Implemented BaseJpaTest, a parent class for every test class.
    - Sets up the EntityFactoryManager, EntityManager and EntityTransaction object, for each test case.
    - Make EntityFactoryManager static cause its a thread safe object and heavy to create each time.
    - EntityManager and EntityTransactions are non-static, since each session has its own states and data, means not thread safe.
    - Implemented PersistAndFetch, performs the persist, commit, clear and fetch operations for each test.
    - Overall this class prevents a lot of repetitive code.

---
+++ src/test/java/in/lokeshkaushik/expensemanager/utils/BaseJpaTest.java
@@ -0,0 +1,48 @@
+package in.lokeshkaushik.expensemanager.utils;
+
+import in.lokeshkaushik.expensemanager.model.base.Identifiable;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+public abstract class BaseJpaTest {
+    protected static EntityManagerFactory entityManagerFactory;
+    protected EntityManager entityManager;  // not static so we can have separate for each session
+    protected EntityTransaction entityTransaction;
+
+    @BeforeAll
+    static void init(){
+        entityManagerFactory = Persistence.createEntityManagerFactory("test-persistence");
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        if(entityManagerFactory != null) entityManagerFactory.close();
+    }
+
+    @BeforeEach
+    void setUp() {
+        entityManager = entityManagerFactory.createEntityManager();
+        entityTransaction = entityManager.getTransaction();
+    }
+
+    @AfterEach
+    void tearDown() {
+        if(entityManager != null) entityManager.close();
+    }
+
+    protected<T extends Identifiable> T PersistAndFetch(Class<T> tClass, T entity) {
+        entityTransaction.begin();
+        entityManager.persist(entity);
+        entityTransaction.commit();
+
+        entityManager.clear(); // force fresh fetch
+
+        return entityManager.find(tClass, entity.getId());
+    }
+}
