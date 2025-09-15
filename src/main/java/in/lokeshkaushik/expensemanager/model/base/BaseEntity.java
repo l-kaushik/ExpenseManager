9bb306040cf930ba5fce1d34b18ce998d8465144
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2025-09-15 21:14:04 +0530

    feat: Implemented BaseEntity class.
    - Provided an automatic timestamp handling system.
    - Changed timestamp type from "LocalDateTime" to "Instant", so every server have same time, no matter the location.
    - On application layer, will convert to local time zone for convenience.

---
+++ src/main/java/in/lokeshkaushik/expensemanager/model/base/BaseEntity.java
@@ -0,0 +1,38 @@
+package in.lokeshkaushik.expensemanager.model.base;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public class BaseEntity {
+
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @Column(nullable = false)
+    private Instant updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = Instant.now();
+        this.updatedAt = Instant.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = Instant.now();
+    }
+
+    public Instant getCreatedAt(){
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt(){
+        return updatedAt;
+    }
+}
