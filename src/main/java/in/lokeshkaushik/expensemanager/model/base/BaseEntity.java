31d5b6681f12659f7bf727a535714a9bf02ccd3b
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2025-09-16 20:50:49 +0530

    feat(base-entity): add UUID field for unique identification
    Each BaseEntity (and its child entities) now includes a UUID field to ensure globally unique identification. This avoids potential conflicts with IDs
    generated later by @GeneratedValue.

--- src/main/java/in/lokeshkaushik/expensemanager/model/base/BaseEntity.java
+++ src/main/java/in/lokeshkaushik/expensemanager/model/base/BaseEntity.java
@@ -1,5 +1,6 @@
 package in.lokeshkaushik.expensemanager.model.base;
 
+import in.lokeshkaushik.expensemanager.model.user.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.PrePersist;
@@ -7,6 +8,7 @@ import jakarta.persistence.PreUpdate;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @MappedSuperclass
 public class BaseEntity {
@@ -16,6 +18,9 @@ public class BaseEntity {
 
     @Column(nullable = false)
     private Instant updatedAt;
+
+    @Column(unique = true, nullable = false, updatable = false)
+    private final String uuid = UUID.randomUUID().toString();
 
     @PrePersist
     protected void onCreate() {
@@ -34,5 +39,21 @@ public class BaseEntity {
 
     public Instant getUpdatedAt(){
         return updatedAt;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseEntity baseEntity = (BaseEntity) o;
+        return uuid.equals(baseEntity.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return uuid.hashCode();
     }
 }
